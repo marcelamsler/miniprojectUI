eccf77aa14faf54cca06c26e6bbdf06cb9476d1d
marcelamsler <marcel.amsler@gmail.com>
2013-10-27 18:21:47 +0100

    added new Look and Feel

--- src/application/LibraryApp.java
+++ src/application/LibraryApp.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.GregorianCalendar;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -27,6 +29,17 @@ public class LibraryApp {
 	public static void main(String[] args) throws Exception {
 		Library library = new Library();
 		initLibrary(library);
+		try {
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
+		} catch (Exception e) {
+		    // If Nimbus is not available, you can set the GUI to another look and feel.
+		}
+		
 		StartWindow window = new StartWindow(library);
 	}
 
