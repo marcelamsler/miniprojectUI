e582da80e1ede67d4f94c4ae8477c555a217f3f9
marcelamsler <marcel.amsler@gmail.com>
2013-12-01 12:00:42 +0100

    added close on press ESC

--- src/view/ListenerJFrame.java
+++ src/view/ListenerJFrame.java
@@ -1,11 +1,13 @@
 package view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.Observable;
 import java.util.Observer;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 import controller.WindowController;
 import domain.Library;
@@ -20,6 +22,14 @@ public abstract class ListenerJFrame extends JFrame implements Observer, WindowL
 		this.windowCtrl = windowCtrl;
 		library.addObserver(this);
 		addWindowListener(this);
+        getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
+                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "Cancel"); //$NON-NLS-1$
+        getRootPane().getActionMap().put("Cancel", new AbstractAction(){ //$NON-NLS-1$
+            public void actionPerformed(ActionEvent e)
+            {
+                dispose();
+            }
+        });
 	}
 
 	@Override
